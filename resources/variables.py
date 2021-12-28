0b112439e13b4db3b1e13ca1743b5bc02f9ef357
ghana-bambale <ghanshyam.moscot@gmail.com>
2021-12-28 19:57:09 +0530

    variable file support added

---
+++ resources/variables.py
@@ -0,0 +1,12 @@
+SHOP_PAGE_URL = "https://qentinelqi.github.io/shop/"
+T_SHIRT_LOCATOR = ''
+PRICE_LOCATOR = ''
+T_SHIRT_TO_PURCHASE = "Gerald the Giraffe"
+T_SHIRT_TO_TEST_COLOR_OPTIONS = "Bumble the Elephant"
+SHOP_PAGE_HEADING = "//*[text()='Find your spirit animal']"
+TOTAL_LOCATOR = "//span[text()='Total']/following-sibling::span"
+SHIRT_COLOR_BEIGE = "//div[@class='style-picker']/div[@style='background-color: #dfd3c2']"
+SHIRT_COLOR_GREEN = "//div[@class='style-picker']/div[@style='background-color: #67aa79']"
+
+COPADO_TEST_PLATFORM = "https://robotic.copado.com/"
+COPADO_LOGIN = "//img[@title='Copado Robotic Testing']"
